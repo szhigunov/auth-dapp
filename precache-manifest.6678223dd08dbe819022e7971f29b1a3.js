5964d96991ed62d24334f17b8a8fda7785a39165
Sergey Zhigunov <i.sergey.zhigunov@gmail.com>
2019-09-22 13:53:29 +0300

    Updates

---
+++ precache-manifest.6678223dd08dbe819022e7971f29b1a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9c9d808cc4d40388f2a48fdd8bc5aa",
+    "url": "/auth-dapp/index.html"
+  },
+  {
+    "revision": "7ff07f4e860952f8039a",
+    "url": "/auth-dapp/static/css/main.e04360f9.chunk.css"
+  },
+  {
+    "revision": "ab45d81d668df99e0d1b",
+    "url": "/auth-dapp/static/js/2.e2473b17.chunk.js"
+  },
+  {
+    "revision": "7ff07f4e860952f8039a",
+    "url": "/auth-dapp/static/js/main.c7eb22ef.chunk.js"
+  },
+  {
+    "revision": "a8df753134da6dad1d08",
+    "url": "/auth-dapp/static/js/runtime-main.9c0c8794.js"
+  }
+]);
