12d9c7b31b0a3a27f523de120bfd28cd36f9b244
Sergey Zhigunov <i.sergey.zhigunov@gmail.com>
2019-09-22 15:40:31 +0300

    Updates

--- precache-manifest.ba23addaf5d541eb354f8a15d421c513.js
+++ precache-manifest.ba23addaf5d541eb354f8a15d421c513.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68d2e6cbdd51fc21ec9054b0110eb0ad",
+    "revision": "f93c010852fb9605a77d934e1b71366a",
     "url": "/auth-dapp/index.html"
   },
   {
-    "revision": "57c30498252402da278e",
+    "revision": "667cfb9fc13595d0057f",
     "url": "/auth-dapp/static/css/main.e04360f9.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/auth-dapp/static/js/2.ea2b73e2.chunk.js"
   },
   {
-    "revision": "57c30498252402da278e",
-    "url": "/auth-dapp/static/js/main.4e51941d.chunk.js"
+    "revision": "667cfb9fc13595d0057f",
+    "url": "/auth-dapp/static/js/main.64c262fa.chunk.js"
   },
   {
     "revision": "a8df753134da6dad1d08",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/auth-dapp/precache-manifest.d5446963e6edbc56cd38a4ba9609330a.js"
+  "/auth-dapp/precache-manifest.ba23addaf5d541eb354f8a15d421c513.js"
 );
 
 self.addEventListener('message', (event) => {
